e31d6d8d15a867abd74f909e5632cf4e1bb59471
ShingiroRoger <srogerverdique@gmail.com>
2020-11-24 11:01:03 +0200

    codelabs

---
+++ HelloToastChallenge217026648/app/src/main/java/com/example/hellotoastchallenge217026648/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.hellotoastchallenge217026648;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private int counter=0;
+    private TextView mshowView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mshowView=(TextView) findViewById(R.id.textView3);
+        if (mshowView!=null){
+            mshowView.setText(Integer.toString(counter));
+        }
+    }
+    public void showToast(View view) {
+        Toast.makeText(this, "Hello Toast", Toast.LENGTH_SHORT).show();
+    }
+
+    public void Countup(View view) {
+        ++counter;
+        mshowView.setText(Integer.toString(counter));
+}
